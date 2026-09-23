02ffc15c57c5e0fcaebcc63f70ddc7bf525d0882
Farjana Pinky <farjanapinky350@gmail.com>
2026-09-23 14:10:28 +0600

    clean code

--- lru-cache.js
+++ lru-cache.js
@@ -11,8 +11,7 @@ class LRUCache {
     if (!this.cache.has(key)) {
       return -1;
     }
-    // Key-ti access kora hoyeche, tai eke most recently used korte 
-    // purono entry delete kore abar re-insert korchi.
+    
     const value = this.cache.get(key);
     this.cache.delete(key);
     this.cache.set(key, value);
@@ -21,11 +20,10 @@ class LRUCache {
 
   put(key, value) {
     if (this.cache.has(key)) {
-      // Key aage thekei thakle delete kore update korchi
+      
       this.cache.delete(key);
     } else if (this.cache.size >= this.capacity) {
-      // Capacity par hoye gele shobcheye purono (Least Recently Used) 
-      // key-ti delete kora hocche (Map-er prothom key-ti LRU).
+      
       const leastRecentlyUsedKey = this.cache.keys().next().value;
       this.cache.delete(leastRecentlyUsedKey);
     }
@@ -45,7 +43,7 @@ console.log('put("B", 20)');
 
 console.log('get("A") ->', cache.get("A")); // Output: 10
 
-cache.put("C", 30); // "B" evict hoye jabe karon "A" matro get("A") diye use kora hoyeche
+cache.put("C", 30); 
 console.log('put("C", 30) [Evicts "B"]');
 
 console.log('get("B") ->', cache.get("B")); // Output: -1 (Not Found)
